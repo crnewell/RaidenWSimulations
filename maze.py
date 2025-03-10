0b98d72cfc584e567adec90b8f9f4e86c1d19ed2
lukeclark03 <luke.clark@ufl.edu>
2025-03-09 23:31:06 -0400

    initiated preprocessing of the maze.

--- maze.py
+++ maze.py
@@ -52,6 +52,16 @@ maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1]
 ]
 
+# initiate preprocessing:
+# create the tree according to the graph, 
+#  have a matrix of pointers in the shape of the maze to the nodes corresponding to the tree
+# color the maze with corresponding colors to the maze
+#  
+
+
+
+
+
 #starting position
 player_pos = [0, 1]  
 root_pos = TREE_X_OFFSET
