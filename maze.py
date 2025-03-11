00d6667555e8aa709b2fb7469fde7b78b00f6d3e
lukeclark03 <luke.clark@ufl.edu>
2025-03-11 08:56:39 -0400

    Created both DFS and BFS

--- maze.py
+++ maze.py
@@ -14,6 +14,8 @@ class Node:
     left_domain: int
     right_domain:int
     children: List["Node"] = field(default_factory=list)
+    is_start: bool = False
+    is_end: bool = False
 
 @dataclass
 class Tile:
@@ -36,12 +38,15 @@ TREE_CURSOR_MULTIPLIER = 2  # this is a multiplier to get the size of the red sq
 # Button constants
 BUTTON_WIDTH = 150
 BUTTON_HEIGHT = 50
-BUTTON_X = WINDOW_WIDTH - BUTTON_WIDTH - 20
-BUTTON_Y = WINDOW_HEIGHT - BUTTON_HEIGHT - 20
+BUTTON_X_BFS = WINDOW_WIDTH - BUTTON_WIDTH - 20
+BUTTON_Y_BFS = WINDOW_HEIGHT - BUTTON_HEIGHT - 20
+BUTTON_X_DFS = WINDOW_WIDTH - BUTTON_WIDTH - 20
+BUTTON_Y_DFS = WINDOW_HEIGHT - BUTTON_HEIGHT - 80  # Position above BFS button
 BUTTON_COLOR = (100, 100, 200)
 BUTTON_HOVER_COLOR = (120, 120, 220)
 BUTTON_TEXT_COLOR = (255, 255, 255)
-BUTTON_TEXT = "Solve Maze"
+BUTTON_TEXT_BFS = "Solve BFS"
+BUTTON_TEXT_DFS = "Solve DFS"
 
 WHITE = (255, 255, 255)
 GREY = (128, 128, 128)
@@ -51,7 +56,7 @@ BLUE = (0, 0, 255)  # Up move
 GREEN = (0, 255, 0)  # Down move
 YELLOW = (255, 255, 0)  # Left move
 PURPLE = (128, 0, 128)  # Right move
-ORANGE = (255, 165, 0)  # For visited cells during BFS
+ORANGE = (255, 165, 0)  # For visited cells during BFS/DFS
 LIGHT_BLUE = (173, 216, 230)  # For frontier cells
 
 COLORS = {
@@ -117,6 +122,10 @@ maze = [
 
 tile_map = {}  # is a dictionary that goes from a location to a tile, with it's color and corresponding tree node
 node_map = {}  # is a dictionary that goes from a location to a node
+
+# Define start and end positions
+start_pos = [0, 1]
+end_pos = [GRID_SIZE - 1, GRID_SIZE - 2]  # Based on the maze layout
 
 # count number of neighbors for each location in the maze
 neighbor_count = [[0 for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
@@ -194,7 +203,9 @@ def add_node(xpos: int, ypos: int, new_Node: bool, parent=None):
     
     # if we were told to make a new node at this location, we must add it, otherwise we use the parent node
     if new_Node:
-        new_node = Node(xpos, ypos, tile_map[(xpos, ypos)].color, 0, 0, 0, 0)
+        is_start = (xpos == start_pos[0] and ypos == start_pos[1])
+        is_end = (xpos == end_pos[0] and ypos == end_pos[1])
+        new_node = Node(xpos, ypos, tile_map[(xpos, ypos)].color, 0, 0, 0, 0, is_start=is_start, is_end=is_end)
         # add the child to the parent node (if it exists)
         if parent != None:
             parent.children.append(new_node)
@@ -248,10 +259,10 @@ update_pos(node_map[(0, 1)], TREE_X_OFFSET, TREE_NODE_RADIUS * TREE_CURSOR_MULTI
 
 # START DISPLAY
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("15x15 Maze Solver")
+pygame.display.set_caption("15x15 Maze Solver - BFS/DFS")
 font = pygame.font.SysFont('Arial', 20)
 
-# For BFS exploration visualization
+# For BFS/DFS exploration visualization
 visited_cells = set()  # Cells that have been explored
 frontier_cells = set()  # Cells that are in the queue to be explored
 path_cells = set()  # Cells that are part of the final solution
@@ -265,11 +276,17 @@ def draw_maze():
                 # Part of the final solution path
                 color = GREEN
             elif (row, col) in visited_cells:
-                # Visited during BFS exploration
+                # Visited during BFS/DFS exploration
                 color = GREY
             elif (row, col) in frontier_cells:
                 # In the frontier (queue)
                 color = LIGHT_BLUE
+            elif (row, col) == (start_pos[0], start_pos[1]):
+                # Start position
+                color = BLUE
+            elif (row, col) == (end_pos[0], end_pos[1]):
+                # End position
+                color = GREEN
             else:
                 # Normal open cell
                 color = tile_map[(row, col)].color
@@ -288,7 +305,29 @@ def draw_subtree(subtreeroot):
                           (node.disp_xpos, node.disp_ypos), 2)
         draw_subtree(node)
 
-    pygame.draw.circle(screen, subtreeroot.color, (subtreeroot.disp_xpos, subtreeroot.disp_ypos), TREE_NODE_RADIUS)
+    # Draw the node as a circle or triangle depending on if it's a start or end node
+    if subtreeroot.is_start or subtreeroot.is_end:
+        # Draw a triangle for start/end nodes
+        triangle_size = TREE_NODE_RADIUS * 1.5
+        if subtreeroot.is_start:
+            # Draw an upward-pointing triangle for start
+            points = [
+                (subtreeroot.disp_xpos, subtreeroot.disp_ypos - triangle_size),
+                (subtreeroot.disp_xpos - triangle_size, subtreeroot.disp_ypos + triangle_size/2),
+                (subtreeroot.disp_xpos + triangle_size, subtreeroot.disp_ypos + triangle_size/2)
+            ]
+            pygame.draw.polygon(screen, BLUE, points)
+        else:
+            # Draw a downward-pointing triangle for end
+            points = [
+                (subtreeroot.disp_xpos, subtreeroot.disp_ypos + triangle_size),
+                (subtreeroot.disp_xpos - triangle_size, subtreeroot.disp_ypos - triangle_size/2),
+                (subtreeroot.disp_xpos + triangle_size, subtreeroot.disp_ypos - triangle_size/2)
+            ]
+            pygame.draw.polygon(screen, GREEN, points)
+    else:
+        # Draw regular nodes as circles
+        pygame.draw.circle(screen, subtreeroot.color, (subtreeroot.disp_xpos, subtreeroot.disp_ypos), TREE_NODE_RADIUS)
 
 def draw_tree():
     # draw a square for the current node
@@ -301,25 +340,22 @@ def draw_tree():
                                       TREE_NODE_RADIUS * 2 * TREE_CURSOR_MULTIPLIER))
     draw_subtree(node_map[(0, 1)])
 
-def draw_button(hover=False):
+def draw_button(button_x, button_y, button_text, hover=False):
     color = BUTTON_HOVER_COLOR if hover else BUTTON_COLOR
-    pygame.draw.rect(screen, color, (BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT), border_radius=5)
+    pygame.draw.rect(screen, color, (button_x, button_y, BUTTON_WIDTH, BUTTON_HEIGHT), border_radius=5)
     
-    text_surface = font.render(BUTTON_TEXT, True, BUTTON_TEXT_COLOR)
-    text_rect = text_surface.get_rect(center=(BUTTON_X + BUTTON_WIDTH // 2, BUTTON_Y + BUTTON_HEIGHT // 2))
+    text_surface = font.render(button_text, True, BUTTON_TEXT_COLOR)
+    text_rect = text_surface.get_rect(center=(button_x + BUTTON_WIDTH // 2, button_y + BUTTON_HEIGHT // 2))
     screen.blit(text_surface, text_rect)
 
-def is_button_hovered(pos):
+def is_button_hovered(pos, button_x, button_y):
     x, y = pos
-    return BUTTON_X <= x <= BUTTON_X + BUTTON_WIDTH and BUTTON_Y <= y <= BUTTON_Y + BUTTON_HEIGHT
+    return button_x <= x <= button_x + BUTTON_WIDTH and button_y <= y <= button_y + BUTTON_HEIGHT
 
 # Function for BFS - Modified to return exploration history
-def find_path_to_exit():
-    # Define the exit position (bottom right open space)
-    exit_pos = [GRID_SIZE - 1, GRID_SIZE - 2]  # Based on your maze
-    
-    queue = deque([(player_pos[0], player_pos[1], [])]) 
-    visited = set([(player_pos[0], player_pos[1])])
+def find_path_bfs():
+    queue = deque([(start_pos[0], start_pos[1], [])]) 
+    visited = set([(start_pos[0], start_pos[1])])
     
     exploration_history = []  
     
@@ -327,16 +363,16 @@ def find_path_to_exit():
     dir_names = ["UP", "RIGHT", "DOWN", "LEFT"]
     
     # Add initial state
-    exploration_history.append((set(visited), set(), (player_pos[0], player_pos[1])))
+    exploration_history.append((set(visited), set(), (start_pos[0], start_pos[1])))
     
     while queue:
         row, col, path = queue.popleft()
         
         # Check if we reached the exit
-        if row == exit_pos[0] and col == exit_pos[1]:
+        if row == end_pos[0] and col == end_pos[1]:
             # Add the final path to the history
             path_set = set()
-            current_pos = (player_pos[0], player_pos[1])
+            current_pos = (start_pos[0], start_pos[1])
             path_set.add(current_pos)
             
             for move in path:
@@ -376,6 +412,68 @@ def find_path_to_exit():
     
     return [], exploration_history, set()  # No path found
 
+# Function for DFS - Modified to return exploration history
+def find_path_dfs():
+    stack = [(start_pos[0], start_pos[1], [])]
+    visited = set([(start_pos[0], start_pos[1])])
+    
+    exploration_history = []
+    
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    dir_names = ["UP", "RIGHT", "DOWN", "LEFT"]
+    
+    # Add initial state
+    exploration_history.append((set(visited), set(), (start_pos[0], start_pos[1])))
+    
+    while stack:
+        row, col, path = stack.pop()  # DFS uses a stack (pop from end)
+        
+        # Check if we reached the exit
+        if row == end_pos[0] and col == end_pos[1]:
+            # Add the final path to the history
+            path_set = set()
+            current_pos = (start_pos[0], start_pos[1])
+            path_set.add(current_pos)
+            
+            for move in path:
+                if move == "UP":
+                    current_pos = (current_pos[0] - 1, current_pos[1])
+                elif move == "RIGHT":
+                    current_pos = (current_pos[0], current_pos[1] + 1)
+                elif move == "DOWN":
+                    current_pos = (current_pos[0] + 1, current_pos[1])
+                elif move == "LEFT":
+                    current_pos = (current_pos[0], current_pos[1] - 1)
+                path_set.add(current_pos)
+            
+            return path, exploration_history, path_set
+        
+        # Add neighbors to the frontier
+        frontier = set()
+        valid_moves = []
+        
+        # Check all four directions
+        for i, (dr, dc) in enumerate(directions):
+            new_row, new_col = row + dr, col + dc
+            
+            # Check if the new position is valid and not visited
+            if (0 <= new_row < GRID_SIZE and 0 <= new_col < GRID_SIZE and 
+                maze[new_row][new_col] == 0 and (new_row, new_col) not in visited):
+                frontier.add((new_row, new_col))
+                valid_moves.append((new_row, new_col, path + [dir_names[i]]))
+        
+        # Record current state
+        if frontier:
+            exploration_history.append((set(visited), frontier, (row, col)))
+        
+        # Add valid moves to the stack (in reverse order to prioritize UP, RIGHT, DOWN, LEFT)
+        for move in reversed(valid_moves):
+            new_row, new_col, new_path = move
+            stack.append((new_row, new_col, new_path))
+            visited.add((new_row, new_col))
+    
+    return [], exploration_history, set()  # No path found
+
 # Global variables for auto-solving
 solution_path = []
 solving_active = False
@@ -384,16 +482,19 @@ exploration_history = []
 exploration_step = 0
 final_path_set = set()
 in_exploration_phase = False
+current_algorithm = None  # To track whether we're using BFS or DFS
 
 # Main loop
 running = True
 clock = pygame.time.Clock()
 move_direction = None
-button_hover = False
+button_hover_bfs = False
+button_hover_dfs = False
 
 while running:
     mouse_pos = pygame.mouse.get_pos()
-    button_hover = is_button_hovered(mouse_pos)
+    button_hover_bfs = is_button_hovered(mouse_pos, BUTTON_X_BFS, BUTTON_Y_BFS)
+    button_hover_dfs = is_button_hovered(mouse_pos, BUTTON_X_DFS, BUTTON_Y_DFS)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -410,10 +511,10 @@ while running:
         elif event.type == pygame.KEYUP:
             move_direction = None
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if button_hover:
+            if button_hover_bfs:
                 # Reset and start BFS exploration
                 player_pos = original_player_pos.copy()
-                solution_path, exploration_history, final_path_set = find_path_to_exit()
+                solution_path, exploration_history, final_path_set = find_path_bfs()
                 solving_active = True
                 current_step = 0
                 exploration_step = 0
@@ -421,10 +522,25 @@ while running:
                 frontier_cells.clear()
                 path_cells.clear()
                 in_exploration_phase = True
-                print("Solution path:", solution_path)
+                current_algorithm = "BFS"
+                print("BFS Solution path:", solution_path)
                 print(f"BFS exploration: {len(exploration_history)} steps")
+            elif button_hover_dfs:
+                # Reset and start DFS exploration
+                player_pos = original_player_pos.copy()
+                solution_path, exploration_history, final_path_set = find_path_dfs()
+                solving_active = True
+                current_step = 0
+                exploration_step = 0
+                visited_cells.clear()
+                frontier_cells.clear()
+                path_cells.clear()
+                in_exploration_phase = True
+                current_algorithm = "DFS"
+                print("DFS Solution path:", solution_path)
+                print(f"DFS exploration: {len(exploration_history)} steps")
     
-    # Handle BFS exploration visualization
+    # Handle exploration visualization
     if solving_active:
         if in_exploration_phase and exploration_step < len(exploration_history):
             # Update visualization states
@@ -433,7 +549,7 @@ while running:
             frontier_cells = frontier_set
             player_pos = [current_pos[0], current_pos[1]]
             exploration_step += 1
-            pygame.time.delay(100)  # Slow down the visualization
+            pygame.time.delay(200)  # Slow down the visualization
             
             # Check if exploration is complete
             if exploration_step >= len(exploration_history):
@@ -471,12 +587,16 @@ while running:
         if 0 <= new_pos[1] < GRID_SIZE and maze[new_pos[0]][new_pos[1]] == 0:
             player_pos = new_pos
     
+    # Drawing everything 
     # Drawing everything
     screen.fill(WHITE)
     draw_maze()
     draw_player()
     draw_tree()
-    draw_button(button_hover)
+    # draw_button(WINDOW_WIDTH-200, WINDOW_HEIGHT-100, "SOLVE BFS", button_hover_bfs )
+    draw_button(BUTTON_X_BFS, BUTTON_Y_BFS, BUTTON_TEXT_BFS, button_hover_bfs)
+    draw_button(BUTTON_X_DFS, BUTTON_Y_DFS, BUTTON_TEXT_DFS, button_hover_dfs)
+    # draw_button(button_hover_dfs)
 
     # Update the display
     pygame.display.flip()
