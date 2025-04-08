f9858e636ac5079430ad8e81e21dbdfb5d2586ba
lukeclark03 <100618629+lukeclark03@users.noreply.github.com>
2025-04-08 15:14:34 -0400

    Started maze runner front end, created button display.

--- main.py
+++ main.py
@@ -165,6 +165,116 @@ class ClickableSimulation(PyGameQtWidget):
     def set_background_green(self):
         self.bg_color = (30, 100, 30)
 
+class MazeRunner(PyGameQtWidget):
+    # Constants
+    WINDOW_WIDTH = 1200  # Increased width to fit tree
+    WINDOW_HEIGHT = 400
+    GRID_SIZE = 15     # Number of rows and columns in the maze
+    CELL_SIZE = WINDOW_HEIGHT // GRID_SIZE  # Size of each cell
+    TREE_X_OFFSET = (WINDOW_HEIGHT+WINDOW_WIDTH)//2  # Offset for tree visualization
+    TREE_NODE_RADIUS = 10
+    TREE_NODE_OFFSET = 6  # this is a multiplier to space the tree out vertically.
+    TREE_CURSOR_MULTIPLIER = 2  # this is a multiplier to get the size of the red square in the tree
+
+
+
+    def __init__(self, parent=None, width=WINDOW_WIDTH, height=WINDOW_HEIGHT):
+        super().__init__(parent, width, height)
+        WINDOW_WIDTH = width
+        WINDOW_HEIGHT = height
+
+        # Button constants
+        BUTTON_WIDTH = 150
+        BUTTON_HEIGHT = 50
+        BUTTON_X_BFS = WINDOW_WIDTH - BUTTON_WIDTH - 20
+        BUTTON_Y_BFS = WINDOW_HEIGHT - BUTTON_HEIGHT - 20
+        BUTTON_X_DFS = WINDOW_WIDTH - BUTTON_WIDTH - 20
+        BUTTON_Y_DFS = WINDOW_HEIGHT - BUTTON_HEIGHT - 80  # Position above BFS button
+        BUTTON_X_RESET = WINDOW_WIDTH - 2*BUTTON_WIDTH - 40 #position next to the bfs button
+        BUTTON_Y_RESET = WINDOW_HEIGHT - BUTTON_HEIGHT - 20
+        BUTTON_X_PAUSE = WINDOW_WIDTH - 3*BUTTON_WIDTH - 60 #position next to the reset button
+        BUTTON_Y_PAUSE = WINDOW_HEIGHT - BUTTON_HEIGHT - 20
+        BUTTON_X_STEP = WINDOW_WIDTH - 2*BUTTON_WIDTH -40
+        BUTTON_Y_STEP = WINDOW_HEIGHT - BUTTON_HEIGHT - 80
+
+        BUTTON_COLOR = (100, 100, 200)
+        BUTTON_HOVER_COLOR = (120, 120, 220)
+        BUTTON_TEXT_COLOR = (255, 255, 255)
+        BUTTON_TEXT_BFS = 'Solve BFS'
+        BUTTON_TEXT_DFS = 'Solve DFS'
+        BUTTON_TEXT_RESET = 'Reset Maze'
+        BUTTON_TEXT_PAUSE = 'Pause/Play Solve'
+        BUTTON_TEXT_STEP = 'Step Solve'
+        self.buttons = [
+            {
+                'rect': pygame.Rect(BUTTON_X_BFS, BUTTON_Y_BFS, BUTTON_WIDTH, BUTTON_HEIGHT),
+                'color': BUTTON_COLOR,
+                'text': BUTTON_TEXT_BFS,
+                'action': self.set_background_red
+            },
+            {
+                'rect': pygame.Rect(BUTTON_X_DFS, BUTTON_Y_DFS, BUTTON_WIDTH, BUTTON_HEIGHT),
+                'color': BUTTON_COLOR,
+                'text': BUTTON_TEXT_DFS,
+                'action': self.set_background_blue
+            },
+            {
+                'rect': pygame.Rect(BUTTON_X_RESET, BUTTON_Y_RESET, BUTTON_WIDTH, BUTTON_HEIGHT),
+                'color': BUTTON_COLOR,
+                'text': BUTTON_TEXT_RESET,
+                'action': self.set_background_green
+            },
+            {
+                'rect': pygame.Rect(BUTTON_X_PAUSE, BUTTON_Y_PAUSE, BUTTON_WIDTH, BUTTON_HEIGHT),
+                'color': BUTTON_COLOR,
+                'text': BUTTON_TEXT_PAUSE,
+                'action': self.set_background_green
+            },
+            {
+                'rect': pygame.Rect(BUTTON_X_STEP, BUTTON_Y_STEP, BUTTON_WIDTH, BUTTON_HEIGHT),
+                'color': BUTTON_COLOR,
+                'text': BUTTON_TEXT_STEP,
+                'action': self.set_background_green
+            }
+        ]
+
+        self.font = pygame.font.Font(None, 20)
+        self.bg_color = (50, 50, 50)
+
+    def update_simulation(self):
+        self.process_pygame_events()
+        self.surface.fill(self.bg_color)
+
+        for button in self.buttons:
+            pygame.draw.rect(self.surface, button['color'], button['rect'])
+            text = self.font.render(button['text'], True, (255, 255, 255))
+            text_rect = text.get_rect(center=button['rect'].center)
+            self.surface.blit(text, text_rect)
+
+        self.update()
+
+    def process_pygame_events(self):
+        for event in self.pygame_events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Check for button clicks
+                for button in self.buttons:
+                    if button['rect'].collidepoint(event.pos):
+                        button['action']()
+        self.pygame_events = []
+
+    def set_background_red(self):
+        self.bg_color = (100, 30, 30)
+
+    def set_background_blue(self):
+        self.bg_color = (30, 30, 100)
+
+    def set_background_green(self):
+        self.bg_color = (30, 100, 30)
+
+
+
+
+
 
 
 class PygameQtApp(QMainWindow):
@@ -188,7 +298,7 @@ class PygameQtApp(QMainWindow):
         self.simulations = [
             {"name": "Moving Circle", "class": PygameWidget},
             {"name": "Button Demo", "class": ClickableSimulation},
-            {"name": "Visualization 3", "class": None},
+            {"name": "MazeRunner", "class": MazeRunner},
             {"name": "Visualization 4", "class": None},
             {"name": "Visualization 5", "class": None},
             {"name": "Visualization 6", "class": None},
